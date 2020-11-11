21a023770a030a3542914ad8199b502664e6c535
Benjamin Cheng <bencheng200@gmail.com>
2020-11-11 16:07:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,8 +14,8 @@ end
 def assign_rooms(array)
   final = []
   room = 1
-    (array.length).times 
-    final.push("Hello, #{array[room-1]}! You'll be assigned to room #{room}!")
+    array.each do |x|
+    final.push("Hello, #{x}! You'll be assigned to room #{room}!")
     room += 1
   end
 final
